f44b90258fe4af827fd05de2fc8e428ae4eeeee3
HeWenQiang <m18518378551@163.com>
2020-04-01 20:23:54 +0800

    测试能否发布三方库代码

---
+++ sdklibrary/src/main/java/com/chengdudaily/sdklibrary/SdkActivity.java
@@ -0,0 +1,14 @@
+package com.chengdudaily.sdklibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SdkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sdk);
+    }
+}
